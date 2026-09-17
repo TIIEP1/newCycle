92ad77a4115220b32abd8a8f86ffa3e3aae29ec6
IEP-lucas <guildafairytail1@hotmail.com>
2026-09-17 00:26:43 -0300

    commit para subir railway

--- src/main/java/com/NewCycle/cashtrash/config/TokenConfig.java
+++ src/main/java/com/NewCycle/cashtrash/config/TokenConfig.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -12,8 +13,8 @@ import java.util.Optional;
 
 @Component
 public class TokenConfig {
-
-    private String secret = "secret";
+    @Value("${jwt.secret}")
+    private String secret;
 
     public String generateToken(User user){
 
